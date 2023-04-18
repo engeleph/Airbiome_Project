e9c1e2bea76f171c35a44e87b8b5b8236930ed9d
engeleph <philipp.engeler@gmx.ch>
2023-04-18 10:13:59 -0500

    initial push for backup

---
+++ create_table.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser(description='sample name')
+parser.add_argument("--sample", required=True, help="Path to input directory containing file EGC200_TRAIN.txt")
+args = parser.parse_args()
+sample=args.sample
+
+os.chdir("/home/philipp/Documents/Airbiome_Project/nxf-kraken2")
+
+df=pd.read_csv('sample.txt', sep='\t', header=0)
+
+df2=pd.read_csv('table.txt', sep='\t', header=0)
+
+df2=df2.merge(df, how='outer', on='OTU')
+
+df2 = df2.fillna(0)
+
+print(df2)
+
+#np.savetxt(r'/home/philipp/Documents/Airbiome_Project/nxf-kraken2/table.txt', df2)  #fmt='%d'
+df2.to_csv('table.txt',sep='\t',index=False)
