364b6cceea757fbe7f535d65eac9ebfb42826d00
engeleph <philipp.engeler@gmx.ch>
2023-04-24 15:33:11 -0500

    updated files

--- beta_diversity.py
+++ beta_diversity.py
@@ -5,14 +5,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-os.chdir("/home/philipp/Documents/Airbiome_Project/nxf-kraken2")
+#os.chdir("/home/philipp/Documents/Airbiome_Project/nxf-kraken2")
 
-df=pd.read_csv('table.txt', sep='\t', index_col=0)            #load df with samples and number of reads per OTU (genus level)
+df=pd.read_csv('output_test/beta_diversity/table.txt', sep='\t', index_col=0)            #load df with samples and number of reads per OTU (genus level)
 otu=df.index                                        #create array containg genus names
 #df=df.T                                             #invert df so that the columns are otus and rows are samples
 
 m=len(df.columns)
-df2=pd.DataFrame({'samples':[],'weighted jaccard':[]})
+df2=pd.DataFrame({'samples':[],'weighted jaccard':[],'Bray Curtis distance':[], 'Euclidean Distance':[]})
 #colors = []
 for i in range (0,m-1):
     for j in range (i+1,m):
@@ -22,15 +22,19 @@ for i in range (0,m-1):
         samples_df=pd.DataFrame({'x':df[col_name1], 'y':df[col_name2]})
         #union=np.sum(df.loc[df[col_name2]!=0][col_name1]!=0)       #OTUs of samples which have reads in both samples
         #inter=union+np.sum(df.loc[df[col_name2]!=0][col_name1]==0)+np.sum(df.loc[df[col_name1]!=0][col_name2]==0)      #OTUs of samples which have reads in either samples        
+        counter_bc=np.sum(abs(samples_df["x"]-samples_df["y"]))
+        denominator_bc=np.sum(samples_df["x"]+samples_df["y"])
+        bc=round(counter_bc/denominator_bc,5)
+        eu_d=round(np.sqrt(np.sum((samples_df["x"]-samples_df["y"])**2)),3)
         minimum=np.sum(samples_df.min(axis=1))
         maximum=np.sum(samples_df.max(axis=1))
         #jaccard=round(union/inter,5)                    #calculate jaccard index
         jaccard=round(minimum/maximum,5)                 #calculate weighted jaccard index
         jaccard_div=1-jaccard
-        df3=pd.DataFrame({'samples':[col_combined],'weighted jaccard':[jaccard_div]})    #include combined sample names and their jaccard index
+        df3=pd.DataFrame({'samples':[col_combined],'weighted jaccard':[jaccard_div],'Bray Curtis distance':[bc], 'Euclidean Distance':[eu_d]})    #include combined sample names and their jaccard index
         df2=df2.append(df3, ignore_index=True)
 
-df2.to_csv('jaccard.txt', header=True, sep ='\t')
+df2.to_csv('output_test/beta_diversity/beta_metrices.txt', header=True, sep ='\t')
 
 '''
 #os.chdir('/cluster/project/grlab/projects/metasub/results/metasub_rna/results')
--- create_table.py
+++ create_table.py
@@ -11,15 +11,18 @@ sample=args.sample
 
 #os.chdir("/home/philipp/Documents/Airbiome_Project/taxprofiler/Airbiome_Project")
 
-df=pd.read_csv('sample.txt', sep='\t', header=0)
+#load df from current sample counts
+df=pd.read_csv('sample.txt', sep='\t', header=0)    
 
-df2=pd.read_csv('table.txt', sep='\t', header=0)
+#load df from merged sample counts
+df2=pd.read_csv('output_test/beta_diversity/table.txt', sep='\t', header=0)
 
-df2=df2.merge(df, how='outer', on='OTU')
+#join both df based on OTU names 
+df2=df2.merge(df, how='outer', on='OTU')        
 
-df2 = df2.fillna(0)
+#all NA values are replaced by 0 (no counts)
+df2 = df2.fillna(0)             
 
-print(df2)
 
 #np.savetxt(r'/home/philipp/Documents/Airbiome_Project/nxf-kraken2/table.txt', df2)  #fmt='%d'
-df2.to_csv('table.txt',sep='\t',index=False)
+df2.to_csv('output_test/beta_diversity/table.txt',sep='\t',index=False)
