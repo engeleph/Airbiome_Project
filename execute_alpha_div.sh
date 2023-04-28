7bf186952855503bf4141cbb676b6140839bbec1
engeleph <philipp.engeler@gmx.ch>
2023-04-28 15:05:08 -0500

    nice plots in ttest scripts

--- execute_alpha_div.sh
+++ execute_alpha_div.sh
@@ -9,7 +9,11 @@ do
 		sampleName=${in%_db4.metaphlan3.biom}
 		sample=${sampleName##*se_}
 		NXF_VER=21.10.6 nextflow run alpha_div.nf --sampleName "$sampleName" --sample "$sample" --basedir "$PWD" --group "$i"  -with-docker qiime2/core
-		
-		cat $PWD/output_test/group_${i}/alpha_diversity/${sample}_alpha_diversity.tsv | awk '$1=="shannon" {print $2}' >> $PWD/output_test/group_${i}/alpha_diversity/alpha_values.csv
+		tsv=$PWD/output_test/group_${i}/alpha_diversity/${sample}_alpha_diversity.tsv
+		if [ -f "$tsv" ]; then
+    			rm -rf $tsv
+		fi
+	
+		cat $tsv | awk '$1=="shannon" {print $2}' >> $PWD/output_test/group_${i}/alpha_diversity/alpha_values.csv
 	done
 done
