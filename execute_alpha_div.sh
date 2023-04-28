68263809f1414c4a204798c741ad935e604db3ae
engeleph <philipp.engeler@gmx.ch>
2023-04-28 13:19:13 -0500

    Makes file shannon_db.sh unnecessary

--- execute_alpha_div.sh
+++ execute_alpha_div.sh
@@ -9,5 +9,8 @@ do
 		sampleName=${in%_db4.metaphlan3.biom}
 		sample=${sampleName##*se_}
 		NXF_VER=21.10.6 nextflow run alpha_div.nf --sampleName "$sampleName" --sample "$sample" --basedir "$PWD" --group "$i"  -with-docker qiime2/core
+		cat $file | awk '$1=="shannon" {print $2}' > $PWD/output_test/group_${z}/alpha_diversity/alpha_values.csv
 	done
 done
+
+
