2f226f93070a77583d8d88bec8e7b04404f1f168
engeleph <philipp.engeler@gmx.ch>
2023-05-11 10:32:26 -0500

    Accepts pathogen file as input

--- search_pathogens.py
+++ search_pathogens.py
@@ -4,33 +4,29 @@ import numpy as np
 
 path_input="output_test"
 number_groups=len(next(os.walk(path_input))[1])
-pathogens=["Streptococcus pneumoniae", "Aspergillus fumigatus", "human adenovirus C"]
+#pathogens=["Streptococcus pneumoniae", "Aspergillus fumigatus", "human adenovirus C"]
+pathogens=pd.read_csv("pathogens.txt", sep='\t', header=0) 
+#pathogens["present"]="Unknown"
+pathogens.set_index("pathogens",drop=True,inplace=True)
 
-def search_pathogen(file_path, word, pathogen):
+def search_pathogen(file_path, word):
     with open(file_path, 'r') as file:
         # read all content of a file
         content = file.read()
         # check if string present in a file
         if word in content:
-            pathogen="True"
+            return "True"
         else:
-            pathogen="False"
+            return "False"
 
-df=pd.DataFrame({'group':[],'Streptococcus pneumoniae':[],'Aspergillus fumigatus':[],'Human adenovirus C':[]})
 
-for group in range(3,(number_groups+1)):
-    streptococcus="False"
-    aspergillus="False"
-    adenovirus="False"
+for group in range(1,number_groups):
+    pathogens["group_{}".format(group)]="Unknown"
     #df2=pd.read_csv("output_test/group_{}/beta_diversity/phyla_table.txt".format(group), sep='\t', header=0)
+    for i in range(len(pathogens.index)):
+        pathogens["group_{}".format(group)].loc[pathogens.index[i]]=search_pathogen("output_test/group_{}/counting_table/phyla_table.txt".format(group), pathogens.index[i])
     
-    search_pathogen("output_test/group_{}/beta_diversity/phyla_table.txt".format(group), pathogens[0], streptococcus)
-    search_pathogen("output_test/group_{}/beta_diversity/phyla_table.txt".format(group), pathogens[1], aspergillus)
-    search_pathogen("output_test/group_{}/beta_diversity/phyla_table.txt".format(group), pathogens[2], adenovirus)
-    
-    df2=pd.DataFrame({'group':[group],'Streptococcus pneumoniae':[streptococcus],'Aspergillus fumigatus':[aspergillus],'Human adenovirus C':[adenovirus]})
-
-    df=df.append(df2, ignore_index=True)
+print(pathogens)
 
 #checks if output path output_analysis exists and creates it if not
 path_output="output_analysis/pathogens" 
@@ -41,6 +37,5 @@ if not isExist:
 os.chdir(path_output)
 
 
-df.to_csv('pathogens.txt', header=True, sep ='\t')
-        
+pathogens.to_csv('pathogens.txt', header=True, sep ='\t')
 
