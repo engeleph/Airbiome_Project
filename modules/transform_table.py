fad4d40ab4bac0ab3aa48d0db755c72d1716089f
engeleph <80984261+engeleph@users.noreply.github.com>
2023-06-09 11:46:14 -0500

    Delete modules directory

--- modules/transform_table.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv('output_analysis/total_counting_table/table.txt', sep='\t', index_col=0, header=0)  #load table with species counts
-otu=df.index                                        #create array containg genus names
-df=df.T
-df.to_csv('output_analysis/total_counting_table/t_table.txt', header=True, sep ='\t')
